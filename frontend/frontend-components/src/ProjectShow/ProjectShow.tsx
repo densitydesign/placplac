661ebb7240eac0f367f4d43399a686b699407364
Gianpiero <gianpiero.papappicco@be20.it>
2021-12-06 14:35:12 +0100

    fix truncate text on project opage in experiment list

--- frontend/frontend-components/src/ProjectShow/ProjectShow.tsx
+++ frontend/frontend-components/src/ProjectShow/ProjectShow.tsx
@@ -6,6 +6,7 @@ import { ExperimentSection } from "./components/ExperimentSection";
 import React, { ComponentType } from "react";
 import { GlossarySidebar } from "../GlossarySidebar";
 import { GlossaryCategory } from "..";
+import classNames from "classnames";
 interface ProjectProps {
   project: Project;
   basePath: string;
@@ -27,18 +28,23 @@ export const ProjectShow = (props: ProjectProps) => {
         <div className={styles.hero_section_content}>
           <h1>{project.title}</h1>
           {project.short_description && (
-            <TextShow text={project.short_description} />
+            <div className="text-only">
+              <TextShow text={project.short_description} />
+            </div>
           )}
         </div>
       </div>
-      <div className={"section"}>
-        <div className={styles.experiments}>
-          <SectionTitle title={"experiments"} />
-          <div className={styles.experiments_description}>
-            {project.experiments_description && (
-              <TextShow text={project.experiments_description} />
-            )}
-          </div>
+      <div className={styles.section}>
+        <SectionTitle title={"experiments"} />
+        <div
+          className={classNames(
+            styles.section_content,
+            styles.experiments_description
+          )}
+        >
+          {project.experiments_description && (
+            <TextShow text={project.experiments_description} />
+          )}
         </div>
       </div>
       {project.experiments.map((experiment) => (
@@ -49,14 +55,18 @@ export const ProjectShow = (props: ProjectProps) => {
           experiment={experiment}
         />
       ))}
-      <div className={"section"}>
-        <div id="abouttheproject" className={styles.experiments}>
-          <SectionTitle title={"about the project"} />
-          <div className={styles.experiments_description}>
-            {project.long_description && (
-              <TextShow text={project.long_description} />
-            )}
-          </div>
+      <div className={styles.section} id="abouttheproject">
+        <SectionTitle title={"about the project"} />
+        <div
+          className={classNames(
+            styles.section_content_smaller,
+            styles.section_content,
+            "text-only"
+          )}
+        >
+          {project.long_description && (
+            <TextShow text={project.long_description} />
+          )}
         </div>
       </div>
       <GlossarySidebar
--- frontend/frontend-components/src/ProjectShow/components/ExperimentSection/ExperimentSection.tsx
+++ frontend/frontend-components/src/ProjectShow/components/ExperimentSection/ExperimentSection.tsx
@@ -6,6 +6,7 @@ import { renderUrlImage } from "../../../utils";
 import { TagsSliding } from "./components/TagsSliding";
 import styles from "./ExperimentSection.module.css";
 import hand from "../../../assets/hand-pointing.png";
+import LinesEllipsis from "react-lines-ellipsis/lib/html";
 interface ExperimentSectionProps {
   experiment: Experiment;
   basePath: string;
@@ -18,21 +19,28 @@ export const ExperimentSection = ({
 }: ExperimentSectionProps) => {
   return (
     <>
-      {experiment.tags && <TagsSliding strings={experiment.tags} />}
-      <div className={styles.main}>
-        <div
-          className={classNames(styles.section, styles.cover)}
-          style={{
-            backgroundImage: `url('${experiment.cover}')`,
-          }}
-        ></div>
-        <div className={classNames(styles.section, styles.content)}>
-          <Link href={`${basePath}experiments/${experiment.id}`}>
-            <h2>{experiment.title}</h2>
-          </Link>
-          <TextShow text={experiment.description} />
+      <Link href={`${basePath}experiments/${experiment.id}`}>
+        <div className={styles.main}>
+          {experiment.tags && <TagsSliding strings={experiment.tags} />}
+          <div className={styles.content}>
+            <div
+              className={classNames(styles.section, styles.cover)}
+              style={{
+                backgroundImage: `url('${experiment.cover}')`,
+              }}
+            ></div>
+            <div className={classNames(styles.section, styles.description)}>
+              <h2>{experiment.title}</h2>
+              <LinesEllipsis
+                maxLine="8"
+                ellipsis="..."
+                unsafeHTML={experiment.description}
+              />
+              {/* <TextShow text={experiment.description} /> */}
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
       <div className={styles.pointing_hand}>
         <img width="20px" height="auto" src={hand} />
       </div>
