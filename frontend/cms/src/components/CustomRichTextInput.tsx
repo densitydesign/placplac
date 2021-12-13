b691a8f1e3235ca97af06fc8f463976f77ef25f4
GIANPIERO <gianpiero@pop-os.localdomain>
2021-12-13 08:45:48 +0100

    fixing glossary

--- frontend/cms/src/components/CustomRichTextInput.tsx
+++ frontend/cms/src/components/CustomRichTextInput.tsx
@@ -44,6 +44,7 @@ interface CustomRichTextInputProps extends TextInputProps {
   project?: number;
   small?: boolean;
 }
+const delimiter = "@";
 export const CustomRichTextInput = (props: CustomRichTextInputProps) => {
   const dataProvider = useDataProvider();
   const {
@@ -130,69 +131,145 @@ export const CustomRichTextInput = (props: CustomRichTextInputProps) => {
     };
   }, [dataProvider, props.project]);
   const editorRef = useRef<any>(null);
-  const [j, setJ] = useState<number>();
-  console.log(j);
+  const glossaryDivRef = useRef<HTMLDivElement>(null);
   return (
-    <FormControl
-      error={!!(touched && error)}
-      fullWidth={fullWidth}
-      className="ra-rich-text-input"
-      margin={margin}
-    >
-      <InputLabel shrink htmlFor={id} className={classes.label}>
-        <FieldTitle
-          label={label}
-          source={source}
-          resource={resource}
-          isRequired={isRequired}
-        />
-      </InputLabel>
-      <Editor
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        tinymceScriptSrc={
-          process.env.PUBLIC_URL + "/tinymce/js/tinymce/tinymce.min.js"
-        }
-        value={value}
-        onEditorChange={onChange}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            "emoticons",
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table paste code help wordcount",
-          ],
-          toolbar:
-            "undo redo | formatselect | " +
-            "bold italic backcolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat emoticons | help | myCustomToolbarButton",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-          setup: (editor) => {
-            editor.ui.registry.addButton("myCustomToolbarButton", {
-              text: "My Custom Button",
-              onAction: () => setJ(12),
-            });
-          },
-          init_instance_callback: function (editor) {
-            editor.on("keydown", function (e) {
-              console.log(editor.selection.getNode().append("dijoasdjsajdajd"));
-            });
-          },
-        }}
-      />
-      <FormHelperText
-        error={!!error}
-        className={!!error ? "ra-rich-text-input-error" : ""}
+    <>
+      <FormControl
+        error={!!(touched && error)}
+        fullWidth={fullWidth}
+        className="ra-rich-text-input"
+        margin={margin}
       >
-        <InputHelperText
-          error={error}
-          helperText={helperText}
-          touched={!!touched}
-        />
-      </FormHelperText>
-    </FormControl>
+        <InputLabel shrink htmlFor={id} className={classes.label}>
+          <FieldTitle
+            label={label}
+            source={source}
+            resource={resource}
+            isRequired={isRequired}
+          />
+        </InputLabel>
+        <div style={{ position: "relative" }}>
+          <div
+            style={{
+              position: "absolute",
+              display: "none",
+              zIndex: 1,
+              height: "300px",
+              width: "300px",
+              background: "white",
+              border: "1px solid black",
+              top: "0px",
+              left: "0px",
+            }}
+            ref={glossaryDivRef}
+          ></div>
+          <Editor
+            onInit={(evt, editor) => (editorRef.current = editor)}
+            tinymceScriptSrc={
+              process.env.PUBLIC_URL + "/tinymce/js/tinymce/tinymce.min.js"
+            }
+            value={value}
+            onEditorChange={onChange}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "emoticons",
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table paste code help wordcount",
+              ],
+              toolbar:
+                "undo redo | formatselect | " +
+                "bold italic backcolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat emoticons | help | myCustomToolbarButton",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              setup: (editor) => {
+                editor.ui.registry.addButton("myCustomToolbarButton", {
+                  text: "My Custom Button",
+                  onAction: () => {},
+                });
+              },
+              init_instance_callback: function (editor) {
+                function cleanUp() {
+                  if (glossaryDivRef.current)
+                    glossaryDivRef.current.style.display = "none";
+                }
+                editor.on("keydown", function (e) {
+                  switch (e.key) {
+                    case delimiter: {
+                      editor.selection.getSel()!.getRangeAt(0);
+                      const selection = editor.selection.getSel();
+                      const node = editor.selection.getNode() as HTMLElement;
+
+                      if (selection && glossaryDivRef.current) {
+                        const contentArea = editor.getContentAreaContainer();
+                        //position
+                        const contentAreaOffset = {
+                          top: contentArea.offsetTop,
+                          left: contentArea.offsetLeft,
+                        };
+                        const glossaryDiv = glossaryDivRef.current;
+                        const coords = selection
+                          .getRangeAt(0)
+                          .getBoundingClientRect();
+
+                        glossaryDiv.style.top = `${
+                          node.getBoundingClientRect().top +
+                          contentAreaOffset.top +
+                          node.clientHeight
+                        }px`;
+                        let positionLeft = coords.left + contentAreaOffset.left;
+
+                        const container = editor.getContainer();
+                        const containerBounding =
+                          container.getBoundingClientRect();
+
+                        console.log(window.innerWidth);
+                        console.log(
+                          positionLeft +
+                            glossaryDiv.clientWidth +
+                            containerBounding.x +
+                            window.scrollX
+                        );
+
+                        if (
+                          positionLeft +
+                            glossaryDiv.clientWidth +
+                            containerBounding.x +
+                            window.scrollX >
+                          window.innerWidth
+                        )
+                          positionLeft =
+                            window.innerWidth - glossaryDiv.clientWidth;
+                        glossaryDiv.style.left = `${positionLeft}px`;
+                        glossaryDiv.style.display = "block";
+                      }
+                      break;
+                    }
+                    case "Escape": {
+                      cleanUp();
+                    }
+                  }
+                });
+              },
+            }}
+          />
+        </div>
+
+        <FormHelperText
+          error={!!error}
+          className={!!error ? "ra-rich-text-input-error" : ""}
+        >
+          <InputHelperText
+            error={error}
+            helperText={helperText}
+            touched={!!touched}
+          />
+        </FormHelperText>
+      </FormControl>
+    </>
   );
 };
