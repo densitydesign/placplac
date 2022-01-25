35a6978f2d409b6e20acda911422c75910c0d13c
gianpiero papappicco <gianpieropapappicco@MacBook-Pro-di-gianpiero.local>
2022-01-25 14:50:21 +0100

    fixes #70 fixes #50

--- frontend/apps/cms/src/components/BuilderInput/components/EditListExperimentSetup.tsx
+++ frontend/apps/cms/src/components/BuilderInput/components/EditListExperimentSetup.tsx
@@ -1,39 +1,39 @@
-import { Grid } from "@material-ui/core";
+import { Grid } from '@material-ui/core';
 import {
   ArrayInput,
   required,
   SimpleFormIterator,
   TextInput,
-} from "react-admin";
+} from 'react-admin';
 
 export const EditListExperimentSetup = () => {
   return (
-    <>
-      <Grid container direction="column">
-        <Grid item>
-          <TextInput
-            validate={required()}
-            helperText={false}
-            fullWidth
-            source={"title"}
-          />
-        </Grid>
-        <Grid item>
-          <TextInput
-            validate={required()}
-            helperText={false}
-            fullWidth
-            source={"subtitle"}
-          />
-        </Grid>
-        <Grid item>
-          <ArrayInput validate={required()} source={"list"}>
-            <SimpleFormIterator getItemLabel={() => "-"}>
-              <TextInput label={false} multiline fullWidth source="" />
-            </SimpleFormIterator>
-          </ArrayInput>
-        </Grid>
+    <Grid container direction="column">
+      <Grid item>
+        <TextInput
+          validate={required()}
+          helperText={false}
+          fullWidth
+          source={'title_bi'}
+          label="Title"
+        />
       </Grid>
-    </>
+      <Grid item>
+        <TextInput
+          validate={required()}
+          helperText={false}
+          fullWidth
+          source={'subtitle_bi'}
+          label="Subtitle"
+        />
+      </Grid>
+      <Grid item>
+        <ArrayInput label="List" validate={required()} source={'list_bi'}>
+          <SimpleFormIterator getItemLabel={() => '-'}>
+            <TextInput label={false} multiline fullWidth source="" />
+          </SimpleFormIterator>
+        </ArrayInput>
+      </Grid>
+    </Grid>
   );
 };
