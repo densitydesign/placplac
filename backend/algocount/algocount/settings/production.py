7616bb1c2df9dbed71badf8bd289218cfa8300b0
gianpiero papappicco <gianpieropapappicco@MacBook-Pro-di-gianpiero.local>
2022-01-27 15:53:30 +0100

    fixing forbidden

--- backend/algocount/algocount/settings/production.py
+++ backend/algocount/algocount/settings/production.py
@@ -16,40 +16,5 @@ DATABASES = {
     }
 }
 
-LOGGING_CONFIG = None
-
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(module)s:%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'WARNING',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': LOG_PATH + 'supersonica.log',
-            'when': 'D', # this specifies the interval
-            'interval': 1, # defaults to 1, only necessary for other values
-            'backupCount': 10, # how many backup file to keep, 10 days
-            'formatter': 'verbose',
-        },
-
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'WARNING'),
-        }
-    },
-})
-
-
-LOGGER = logging.getLogger('django')
+LOGGER = logging.getLogger()
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
